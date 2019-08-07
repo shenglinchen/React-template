b8d08d488006115c2b42b77c1380623870628472
chenshenglin <chenshenglin@forchange.tech>
2019-08-07 16:17:47 +0800

    feat: 添加优化

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -115,5 +115,28 @@ module.exports = {
             '@library': utils.resolve('src/library'),
         }
     },
-    devtool: 'inline-source-map'
+    devtool: 'inline-source-map',
+    optimization: {
+        splitChunks: {
+            chunks: 'async',
+            minSize: 30000,
+            maxSize: 0,
+            minChunks: 1,
+            maxAsyncRequests: 5,
+            maxInitialRequests: 3,
+            automaticNameDelimiter: '~',
+            name: true,
+            cacheGroups: {
+                vendors: {
+                    test: /[\\/]node_modules[\\/]/,
+                    priority: -10
+                },
+                default: {
+                    minChunks: 2,
+                    priority: -20,
+                    reuseExistingChunk: true
+                }
+            }
+        }
+    }
 }
